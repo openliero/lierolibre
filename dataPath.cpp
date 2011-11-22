3ff7142b0e17b480abb703108bd9d40b7fb1fa43
Arand Nash <ienorand@gmail.com>
2011-11-22 03:45:55 +0100

    basic skeleton hpp and cpp files for datapath

---
+++ dataPath.cpp
@@ -0,0 +1,12 @@
+#include "dataPath.hpp"
+#include <string>
+
+
+string DataPath::path_readonly(string filename);
+{
+return "p/r"
+}
+string DataPath::path_configdir(void);
+{
+return "p/c"
+}
---
+++ dataPath.hpp
@@ -0,0 +1,12 @@
+#ifndef DATA_PATH_HPP
+#define DATA_PATH_HPP
+
+
+class DataPath;
+{
+public:
+	string path_readonly(string filename);
+	string path_configdir();
+}
+
+#endif //DATA_PATH_HPP
