27f1b676d0e721c6a27e2e125f16912de13b50ed
Arand Nash <ienorand@gmail.com>
2011-11-22 03:45:56 +0100

    Implement configdir with a simple mkdir

--- dataPath.cpp
+++ dataPath.cpp
@@ -1,5 +1,10 @@
 // Compile via ~$ g++ -c dataPath.cpp
 #include "dataPath.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 DataPath::DataPath(string a_lieroexe) {
 	lieroexe = a_lieroexe;
@@ -10,5 +15,12 @@ string DataPath::file(string filename) {
 }
 
 string DataPath::configdir(void) {
-	return "/tmp";
+	const string home = "HOME";
+	const string homedir = getenv(home.c_str());
+	const string dotdir = "/.liero";
+	string configdir;
+	configdir.append(homedir);
+	configdir.append(dotdir);
+	mkdir(configdir.c_str(), 0777);
+	return configdir;
 }
