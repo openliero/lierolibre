99d3d69f59aa02b6a24c99d006740d02d5c51b89
Arand Nash <ienorand@gmail.com>
2011-11-22 03:45:56 +0100

    Implement testing for lieroexe

--- boostTesting.cpp
+++ boostTesting.cpp
@@ -15,8 +15,9 @@ using namespace boost::filesystem;
 
 const char *home = "HOME";
 const char *dotdir = "/.liero";
-const char *afile = "/LIERO.DAT";
-const char template_string[] = "/tmp/liero_configdir_tmp_XXXXXX";
+const char *lierodat_fname = "/LIERO.DAT";
+const char *lieroexe_fname = "/LIERO.EXE";
+const char template_string[] = "/tmp/liero_tmp_XXXXXX";
 
 BOOST_AUTO_TEST_CASE(configdir_return_correct_path)
 {
@@ -35,6 +36,8 @@ BOOST_AUTO_TEST_CASE(configdir_return_correct_path)
 	BOOST_CHECK(strcmp(data_path.configdir().c_str(), configdir) == 0);
 
 	remove_all(tempdir);
+
+	printf("### end test configdir_return_correct_path\n\n");
 }
 
 BOOST_AUTO_TEST_CASE(configdir_can_create_file)
@@ -47,17 +50,42 @@ BOOST_AUTO_TEST_CASE(configdir_can_create_file)
 	strcpy(tempdir, template_string);
 	mkdtemp(tempdir);
 	setenv(home, tempdir, 1);
-	printf("tempdir: %s\n", tempdir);
 
 	DataPath data_path("/usr/lib/liero");
 	configdir = data_path.configdir().c_str();
 
 	strcpy(configfile_path, configdir);
-	strcat(configfile_path, afile);
-	printf("configfile_path: %s\n", configfile_path);
+	strcat(configfile_path, lierodat_fname);
 
 	BOOST_CHECK(configfile = fopen(configfile_path, "w+"));
 	fclose(configfile);
 
 	remove_all(tempdir);
+
+	printf("### end test configdir_can_create_file\n\n");
+}
+
+BOOST_AUTO_TEST_CASE(lieroexe_return_correct_path)
+{
+	char tempdir[500];
+	char lieroexe_path[500];
+        FILE *lieroexe;
+
+	strcpy(tempdir, template_string);
+	mkdtemp(tempdir);
+	printf("tempdir: %s\n", tempdir);
+	strcpy(lieroexe_path, tempdir);
+	strcat(lieroexe_path, lieroexe_fname);
+	printf("lieroexe_path: %s\n", lieroexe_path);
+	lieroexe = fopen(lieroexe_path, "w+");
+	fclose(lieroexe);
+
+	DataPath data_path(tempdir);
+
+
+	BOOST_CHECK(strcmp(data_path.file("LIERO.EXE").c_str(), lieroexe_path) == 0);
+
+	remove_all(tempdir);
+
+	printf("### end test lieroexe_return_correct_path\n\n");
 }
