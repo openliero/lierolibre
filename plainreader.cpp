65d8c59983fb7f72af5f8e8306efe0203e930e92
Martin Erik Werner <martinerikwerner@gmail.com>
2012-03-30 22:42:46 +0200

    use equals sign instead of colon

--- plainreader.cpp
+++ plainreader.cpp
@@ -163,8 +163,8 @@ int stuff(void)
 	}
 
 	fclose(exe);
-	cout << "copyright1: " << copyright1 << endl;
-	cout << "materials: ";
+	cout << "copyright1 =  " << copyright1 << endl;
+	cout << "materials = ";
 	for(int i = 0; i < 256; ++i)
 		cout << materials[i].flags << " ";
 	cout << endl;
