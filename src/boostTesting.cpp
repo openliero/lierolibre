062cc6896f53317084bef318c92bacb3f519c072
Martin Erik Werner <martinerikwerner@gmail.com>
2012-05-01 15:19:05 +0200

    Updated dataPath test with liero.txt

--- src/boostTesting.cpp
+++ src/boostTesting.cpp
@@ -1,30 +1,5 @@
-/*
- * Copyright (c) 2012, Martin Erik Werner <martinerikwerner@gmail.com>
- * This software is released under the The BSD-2-Clause License:
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * * Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 // Compile & run with:
-// ~$ g++ -lboost_unit_test_framework -lboost_system -lboost_filesystem dataPath.o -oboosttest boostTesting.cpp && ./boosttest --loglevel=test_suite
+// ~$ g++ -lboost_unit_test_framework -lboost_system -lboost_filesystem dataPath.o filesystem.o -oboosttest boostTesting.cpp && ./boosttest --loglevel=test_suite
 
 /*
  * These are unit tests for dataPath.
@@ -104,9 +79,11 @@ struct DirectorySetup
 		file_access_map.insert(pair<string, bool>("LIERO.OPT", true));
 		file_access_map.insert(pair<string, bool>("LIERO.DAT", true));
 		file_access_map.insert(pair<string, bool>("liero.cfg", true));
+		file_access_map.insert(pair<string, bool>("liero.txt", true));
 
 		names_rw.push_back("LIERO.DAT");
 		names_rw.push_back("liero.cfg");
+		names_rw.push_back("liero.txt");
 
 		template_string = "/tmp/liero_tmp_XXXXXX";
 		strcpy(c_temp_homedir, template_string.c_str());
