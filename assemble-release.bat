b2a06253045c4f99837b28c42e55e13e2883a179
Martin Erik Werner <martinerikwerner@gmail.com>
2012-06-10 12:10:29 +0200

    Add lgpl plaintext

--- assemble-release.bat
+++ assemble-release.bat
@@ -7,7 +7,7 @@ cd %lldir%
 
 copy ..\src\_bin\Release\lierolibre.exe ..\data ..\scripts\lierolibre-cmd.bat
 
-copy ..\AUTHORS.txt ..\ChangeLog.txt ..\COPYING.txt ..\NEWS.txt ..\README.txt
+copy ..\AUTHORS.txt ..\ChangeLog.txt ..\COPYING.txt ..\NEWS.txt ..\README.txt ..\lgpl-2.1.txt
 
 copy ..\lib\boost\boost_program_options-vc100-mt-1_49.dll ..\lib\msvcp100.dll ..\lib\msvcr100.dll ..\src\_build\Release\libconfig++.dll
 
