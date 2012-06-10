41af8e3372d99cccdcd018a0fa9df58b926d4bae
Martin Erik Werner <martinerikwerner@gmail.com>
2012-06-10 23:28:08 +0200

    Workaround lack of sane cli

--- assemble-release.bat
+++ assemble-release.bat
@@ -1,14 +1,22 @@
-set lldir="%~dp0\lierolibre-0.0_win32"
+SET lldir="%~dp0\lierolibre-0.0_win32"
 
-md %lldir%
-md %lldir%\libsrc
+SET core=src\_bin\Release\lierolibre.exe scripts\lierolibre-cmd.bat
+SET doc=AUTHORS.txt ChangeLog.txt COPYING.txt NEWS.txt README.txt lgpl-2.1.txt
+SET lib=lib\boost\boost_program_options-vc100-mt-1_49.dll lib\msvcp100.dll lib\msvcr100.dll src\_build\Release\libconfig++.dll lib\SDL-1.2.15\VisualC\SDL\Release\SDL.dll
+SET src=lib\SDL-1.2.15 lib\libconfig
 
+MD %lldir%
+MD %lldir%\libsrc
 
-copy ..\src\_bin\Release\lierolibre.exe ..\data ..\scripts\lierolibre-cmd.bat %lldir%
+XCOPY /YIE data %lldir%\data
+DEL %lldir%\data\.git %lldir%\data\LICENSE.TXT
 
-copy ..\AUTHORS.txt ..\ChangeLog.txt ..\COPYING.txt ..\NEWS.txt ..\README.txt ..\lgpl-2.1.txt %lldir%
+FOR %%i IN (%core% %doc% %lib%) DO (
+  COPY %%i %lldir%
+)
 
-copy ..\lib\boost\boost_program_options-vc100-mt-1_49.dll ..\lib\msvcp100.dll ..\lib\msvcr100.dll ..\src\_build\Release\libconfig++.dll ..\lib\SDL-1.2.15\VisualC\SDL\Release\SDL.dll %lldir%
+XCOPY /YIE lib\SDL-1.2.15 %lldir%\libsrc\SDL-1.2.15
+XCOPY /YIE lib\libconfig %lldir%\libsrc\libconfig
 
-copy ..\lib\SDL-1.2.15 ..\lib\libconfig %lldir%\libsrc
+PAUSE
 
