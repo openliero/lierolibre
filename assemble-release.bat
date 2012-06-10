7e3663c0ed4151eb6fc1f9f002daf2c53621f697
Martin Erik Werner <martinerikwerner@gmail.com>
2012-06-10 23:44:23 +0200

    rename to txt, add SDL_mixer.dll

--- assemble-release.bat
+++ assemble-release.bat
@@ -1,8 +1,8 @@
 SET lldir="%~dp0\lierolibre-0.0_win32"
 
 SET core=src\_bin\Release\lierolibre.exe scripts\lierolibre-cmd.bat
-SET doc=AUTHORS.txt ChangeLog.txt COPYING.txt NEWS.txt README.txt lgpl-2.1.txt
-SET lib=lib\boost\boost_program_options-vc100-mt-1_49.dll lib\msvcp100.dll lib\msvcr100.dll src\_build\Release\libconfig++.dll lib\SDL-1.2.15\VisualC\SDL\Release\SDL.dll
+SET doc=AUTHORS ChangeLog.txt COPYING.txt NEWS README.txt lgpl-2.1.txt
+SET lib=lib\boost\boost_program_options-vc100-mt-1_49.dll lib\msvcp100.dll lib\msvcr100.dll "src\_build\Release\libconfig++.dll" lib\SDL-1.2.15\VisualC\SDL\Release\SDL.dll lib\SDL_mixer\lib\x86\SDL_mixer.dll
 SET src=lib\SDL-1.2.15 lib\libconfig
 
 MD %lldir%
@@ -14,6 +14,8 @@ DEL %lldir%\data\.git %lldir%\data\LICENSE.TXT
 FOR %%i IN (%core% %doc% %lib%) DO (
   COPY %%i %lldir%
 )
+MOVE %lldir%\AUTHORS %lldir%\AUTHORS.txt
+MOVE %lldir%\NEWS %lldir%\NEWS.txt
 
 XCOPY /YIE lib\SDL-1.2.15 %lldir%\libsrc\SDL-1.2.15
 XCOPY /YIE lib\libconfig %lldir%\libsrc\libconfig
