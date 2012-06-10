03bfb7913c69b075f3ea8adab64f944396063d36
Martin Erik Werner <martinerikwerner@gmail.com>
2012-06-10 14:31:16 +0200

    Extend windows release script

--- assemble-release.bat
+++ assemble-release.bat
@@ -3,11 +3,13 @@
 set lldir="%~dp0\lierolibre-0.0_win32"
 
 md %lldir%
-cd %lldir%
+md %lldir%\libsrc
 
-copy ..\src\_bin\Release\lierolibre.exe ..\data ..\scripts\lierolibre-cmd.bat
+copy ..\src\_bin\Release\lierolibre.exe ..\data ..\scripts\lierolibre-cmd.bat %lldir%
 
-copy ..\AUTHORS.txt ..\ChangeLog.txt ..\COPYING.txt ..\NEWS.txt ..\README.txt ..\lgpl-2.1.txt
+copy ..\AUTHORS.txt ..\ChangeLog.txt ..\COPYING.txt ..\NEWS.txt ..\README.txt ..\lgpl-2.1.txt %lldir%
 
-copy ..\lib\boost\boost_program_options-vc100-mt-1_49.dll ..\lib\msvcp100.dll ..\lib\msvcr100.dll ..\src\_build\Release\libconfig++.dll
+copy ..\lib\boost\boost_program_options-vc100-mt-1_49.dll ..\lib\msvcp100.dll ..\lib\msvcr100.dll ..\src\_build\Release\libconfig++.dll %lldir%
+
+copy ..\lib\SDL-1.2.15 ..\lib\libconfig %lldir%\libsrc
 
