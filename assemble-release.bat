5680c14ac19c30751b0b4bb890cec97e3408e2b7
Martin Erik Werner <martinerikwerner@gmail.com>
2012-06-11 13:48:58 +0200

    Rename liero.cfg->lierocfg.txt in release script

--- assemble-release.bat
+++ assemble-release.bat
@@ -10,6 +10,7 @@ MD %lldir%\libsrc
 
 XCOPY /YIE data %lldir%\data
 DEL %lldir%\data\.git %lldir%\data\LICENSE.TXT
+MOVE %lldir%\data\liero.cfg %lldir%\data\lierocfg.cfg
 
 FOR %%i IN (%core% %doc% %lib%) DO (
   COPY %%i %lldir%
