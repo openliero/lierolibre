8e4e2c02b78bf124a7dc16871dbb939f29410830
Martin Erik Werner <martinerikwerner@gmail.com>
2012-10-27 00:54:49 +0200

    documentation and script fixes
    * Remove windows ChangeLog (since windows branch is merged)
    * Include more windows files in source tarball
    * Move scripts to scripts/

--- scripts/assemble-release.bat
+++ scripts/assemble-release.bat
@@ -1,7 +1,7 @@
 SET lldir="%~dp0\lierolibre-0.0_win32"
 
 SET core=src\_bin\Release\lierolibre.exe scripts\lierolibre-cmd.bat
-SET doc=AUTHORS ChangeLog.txt COPYING.txt NEWS README.txt lgpl-2.1.txt
+SET doc=AUTHORS ChangeLog COPYING.txt NEWS README.txt lgpl-2.1.txt
 SET lib=lib\boost\boost_program_options-vc100-mt-1_49.dll lib\msvcp100.dll lib\msvcr100.dll "src\_build\Release\libconfig++.dll" lib\SDL-1.2.15\VisualC\SDL\Release\SDL.dll lib\SDL_mixer\lib\x86\SDL_mixer.dll
 SET src=lib\SDL-1.2.15 lib\libconfig
 
@@ -17,6 +17,7 @@ FOR %%i IN (%core% %doc% %lib%) DO (
 )
 MOVE %lldir%\AUTHORS %lldir%\AUTHORS.txt
 MOVE %lldir%\NEWS %lldir%\NEWS.txt
+MOVE %lldir%\ChangeLog %lldir%\ChangeLog.txt
 
 XCOPY /YIE lib\SDL-1.2.15 %lldir%\libsrc\SDL-1.2.15
 XCOPY /YIE lib\libconfig %lldir%\libsrc\libconfig
